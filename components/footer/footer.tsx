f12ccbba0c7f6003eb530815bc6979a9d5962482
Enyel Sequeira <enyelsequeira@hotmail.com>
2021-04-08 01:07:03 +0800

    adding footer done

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -27,15 +27,18 @@ const Footer = () => {
             <li className="font-body font-light">Testimonials</li>
           </div>
 
-          <div className="my-2">
-            <p className="font-display font-bold">Title</p>
-            <li className="font-body font-light">story</li>
-            <li className="font-body font-light">stack</li>
-            <li className="font-body font-light">Testimonials</li>
+          <div className="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+            <div className="text-red-light mb-2">Newsletter</div>
+            <p className="text-grey-darker leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, consectetur. </p>
+            <div className="mt-4 flex">
+              <input type="text" className="p-2 border border-grey-light round text-grey-dark text-sm h-auto" placeholder="Your email address" />
+              <button className="border-2 bg-indigo-400 rounded-sm h-auto text-xs p-3 ml-3">Subscribe</button>
+            </div>
           </div>
         </ul>
       </div>
-      <p className="uppercase font-display font-extrabold text-center my-4">  All rights reserved E.S.codes © {new Date().getFullYear()}{' '} </p>
+      <p className="uppercase font-display font-extrabold text-center my-4">   &copy; E.S.CODES. All rights reserved {new Date().getFullYear()}{' '} </p>
+
 
     </div>
   )
