3d4d45d86af75d57c0eefdd5ab459c657f131877
Enyel Sequeira <enyelsequeira@hotmail.com>
2021-04-08 02:09:21 +0800

    adding input

--- components/search-input/search-input.tsx
+++ components/search-input/search-input.tsx
@@ -10,7 +10,7 @@ const SearchInput:FC<Props> = ({setSearchValue}) => {
 
 
   return (
-    <div className="bg-white dark:bg-gray-500 shadow p-4 w-2/4 flex my-2 mx-auto rounded-lg">
+    <div className="bg-white dark:bg-gray-500 shadow p-2 md:p-4 w-full md:w-2/4 flex my-2 mx-auto rounded-lg">
       <span className="w-auto flex justify-end items-center text-gray-500 dark:text-white p-2">
         <AiOutlineSearch className="text-3xl" />
       </span>
